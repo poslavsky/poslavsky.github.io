5c16025a2d43bb991894e4563964ae42b38acd8e
Ppslavsky <m.poslavski@napishem.com>
2016-07-12 17:01:56 +0300

    add js content upload

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,60 +1,46 @@
 'use strict';
-
 (function() {
 	var container = document.querySelector('.catalog');
-	var tamplate = document.querySelector('#template');
-	var jsonData = [];
+	var templateElement = document.querySelector('#template');
+	var elementToClone;
+	var itemsList = [];
+	var ITEMS_LOAD_URL = '//poslavsky.github.io/content.json';
 
-	function getData() {
-		console.log('111');
-		var xhr = new XMLHttpRequest();
-		xhr.onloadstart = function() {
-      container.classList.add('pictures-loading');
-
-    };
-		xhr.open('GET', '//poslavsky.github.io/content.json');
-		xhr.onload = function(e) {
-      container.classList.remove('pictures-loading');
-      var rawData = e.target.response;
-      var loadedPictures = JSON.parse(rawData);
-      parseData = loadedPictures;
-			console.log(rawData);
-      renderPictures(loadedPictures);
-    };
-		xhr.send();
-		xhr.onerror = function() {
-      container.classList.add('pictures-failure');
-    };
-		xhr.timeout = 10000;
-    xhr.ontimeout = function() {
-      container.classList.add('pictures-failure');
-    };
-	};
-
-	getData();
-
-	function getElementFromTamplate(data) {
-		var element;
-		if ('content' in template) {
-      element = template.content.childNodes[1].cloneNode(true);
-    } else {
-      element = template.children[0].cloneNode(true);
-    }
-
-		element.querySelector('.card__title').textContent = data.title;
-    element.querySelector('.card__descr').textContent = data.descr;
-		element.querySelector('.card__tag').textContent = data.tag;
-
-		backgroundImage.src = data.url;
+	if('content' in templateElement) {
+		elementToClone = templateElement.content.querySelector('.card');
+	} else {
+		elementToClone = templateElement.querySelector('.card');
 	}
 
-	function renderPictures(picturesArray) {
-    container.innerHTML = '';
-    var fragment = document.createDocumentFragment();
-    picturesArray.forEach(function(picture) {
-      var element = getElementFromTemplate(picture);
-      fragment.appendChild(element);
-    });
-    container.appendChild(fragment);
-  }
+	var getItemElement = function(data, itemContainer) {
+		var element = elementToClone.cloneNode(true);
+		element.querySelector('.card__title').textContent = data.title;
+    element.querySelector('.card__descr').textContent = data.descr;
+		element.querySelector('.card__tag').textContent = data.tags;
+		element.querySelector('.card__img').src = ('img/pic/'+data.pic1);
+		container.appendChild(element);
+  	return element;
+	}
+
+	var getItems = function(callback) {
+		var xhr = new XMLHttpRequest();
+		xhr.onload = function(evt) {
+	    var loadedData = JSON.parse(evt.target.response);
+	    callback(loadedData);
+	  };
+
+		xhr.open('GET', ITEMS_LOAD_URL);
+  	xhr.send();
+	};
+
+	var renderItems = function(itemsList) {
+		itemsList.forEach(function(item) {
+			getItemElement(item, container);
+		});
+	};
+
+	getItems(function(loadedItems) {
+		itemsList = loadedItems;
+		renderItems(itemsList);
+	})
 })();
